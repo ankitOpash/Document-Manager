4533a9598a63cba8ce470b8ff30578ce6593c111
ankitOpash <ay.opash@gmail.com>
2024-11-26 10:15:14 +0530

    add basic setup of frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { DocumentList } from './components/DocumentList';
+import { SearchBar } from './components/SearchBar';
+import { AddDocumentForm } from './components/AddDocumentForm';
+import { DocumentEditor } from './components/DocumentEditor';
+import { useDocuments } from './hooks/useDocuments';
+import { Document } from './types/document';
+import { FileText } from 'lucide-react';
+
+function App() {
+ 
+
+
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+    <h1>Frontend code</h1>
+    </div>
+  );
+}
+
+export default App;
